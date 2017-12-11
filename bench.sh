461f2bea2257c9201dcbddc78ddf4f33ab2b0675
cohalz <cohalzweb@gmail.com>
2017-12-11 07:51:31 +0000

    reloadさせる設定を増やす

--- bench.sh
+++ bench.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo rm /var/log/nginx/access.log
+sudo sysctl -p
+sudo /etc/init.d/mysql reload
 sudo service nginx reload
 ./benchmarker bench
 sudo cat /var/log/nginx/access.log | ./kataribe
