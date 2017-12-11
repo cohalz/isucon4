79888f89064bbd76c6383e1c13c6c11d6970e091
cohalz <cohalzweb@gmail.com>
2017-12-11 18:20:35 +0900

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -2,7 +2,7 @@
 
 sudo rm /var/log/nginx/access.log
 sudo sysctl -p
-sudo /etc/init.d/mysql reload
+sudo /etc/init.d/mysqld restart
 sudo service nginx reload
 ./benchmarker bench
 sudo cat /var/log/nginx/access.log | ./kataribe
