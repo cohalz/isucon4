87697fe015f6bd943515884d2e0ff6d4713154fd
cohalz <cohalzweb@gmail.com>
2017-12-15 05:43:14 +0000

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-cd isucon4
 wget https://github.com/matsuu/kataribe/releases/download/v0.3.0/linux_386.zip 
 unzip linux_386.zip
+
 sudo rm /etc/sysctl.conf
 sudo ln -s /home/isucon/isucon4/settings/sysctl.conf /etc/sysctl.conf
 sudo rm /etc/nginx/nginx.conf
 sudo ln -s /home/isucon/isucon4/settings/nginx.conf /etc/nginx/nginx.conf
 sudo rm /etc/supervisord.conf 
 sudo ln -s /home/isucon/isucon4/settings/supervisord.conf /etc/supervisord.conf
-sudo rm /etc/my.cnf
-sudo ln -s /home/isucon/isucon4/settings/my.cnf /etc/my.cnf
+ps aux | grep foreman | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+sudo /etc/init.d/supervisord restart
+#sudo rm /etc/my.cnf
+#sudo ln -s /home/isucon/isucon4/settings/my.cnf /etc/my.cnf
 sudo ln -s /home/isucon/benchmarker /home/isucon/isucon4/benchmarker
