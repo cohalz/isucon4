6a4caf1b25394d83ffaa912095a4f64bc9240f70
cohalz <cohalzweb@gmail.com>
2017-12-15 05:44:27 +0000

    add my.cnf

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ sudo rm /etc/sysctl.conf
 sudo ln -s /home/isucon/isucon4/settings/sysctl.conf /etc/sysctl.conf
 sudo rm /etc/nginx/nginx.conf
 sudo ln -s /home/isucon/isucon4/settings/nginx.conf /etc/nginx/nginx.conf
+sudo rm /etc/my.cnf
+sudo ln -s /home/isucon/isucon4/settings/my.cnf /etc/my.cnf
 sudo rm /etc/supervisord.conf 
 sudo ln -s /home/isucon/isucon4/settings/supervisord.conf /etc/supervisord.conf
 ps aux | grep foreman | grep -v grep | awk '{ print "kill -9", $2 }' | sh
 sudo /etc/init.d/supervisord restart
-#sudo rm /etc/my.cnf
-#sudo ln -s /home/isucon/isucon4/settings/my.cnf /etc/my.cnf
 sudo ln -s /home/isucon/benchmarker /home/isucon/isucon4/benchmarker
