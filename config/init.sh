9d005e8a4188a3d9fed3f652a27129747fb94c3b
cohalz <cohalzweb@gmail.com>
2017-12-15 07:27:34 +0000

    add index

--- config/init.sh
+++ config/init.sh
@@ -9,5 +9,7 @@ myhost=127.0.0.1
 myport=3306
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
+#indexを追加するalter table <table名> add index <index名> (<column名>)で追加する
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} -e "alter table login_log add index ip (ip), add index user_id (user_id);"
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
